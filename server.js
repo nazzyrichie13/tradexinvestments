ce4cab5daf46514ac13a236d8e8c5adfdec7dd92
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-10 14:07:23 +0100

    modify

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // ====== Middleware ======
-pp.use(cors({
+app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
