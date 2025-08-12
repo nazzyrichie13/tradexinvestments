d18230335aab44e946a88d616c78344b7bdee060
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-12 21:51:08 +0100

    change amd modify for deploy

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/api/contact', async (req, res) => {
 });
 
 // ====== Connect to MongoDB ======
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
