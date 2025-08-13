21d96b44d0e89c5fadfd0280cc403309ee697a82
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-13 10:09:08 +0100

    Your descriptive commit message

---
+++ testMongo.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.error('Connection error:', err));
