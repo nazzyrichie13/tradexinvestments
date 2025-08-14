a2ca84c998429a760d9ab5b79e92662d7522cc75
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-14 01:39:56 +0100

    modify

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 // Routes
 app.use("/api/auth", authRoutes);
