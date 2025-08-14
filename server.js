74083f78b6fc14e36341fde72bc58f9379733dd1
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-14 04:01:14 +0100

    modify

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
