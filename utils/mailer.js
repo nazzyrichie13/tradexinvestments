525b4c45f63aa64396d107a6909639150febb420
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-15 15:24:14 +0100

    modify

--- utils/mailer.js
+++ utils/mailer.js
@@ -2,28 +2,37 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,       // Your email address from environment
-    pass: process.env.EMAIL_PASS        // Your app password or real password from environment
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
-transporter.verify((error, success) => {
+transporter.verify((error) => {
   if (error) {
     console.error('Error configuring mailer:', error);
   } else {
     console.log('Mailer is ready to send messages');
   }
 });
+
 export const send2FACode = async (toEmail, code) => {
   await transporter.sendMail({
     from: `"TradexInvest" <${process.env.SMTP_USER}>`,
     to: toEmail,
     subject: "Your 2FA Code",
     html: `<p>Your 2FA code is: <strong>${code}</strong></p>`
+  });
+};
+
+export const sendMail = async (toEmail, message) => {
+  await transporter.sendMail({
+    from: `"TradexInvest" <${process.env.SMTP_USER}>`,
+    to: toEmail,
+    subject: "Notification",
+    text: message
   });
 };
 
