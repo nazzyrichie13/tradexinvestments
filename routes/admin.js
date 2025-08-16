7b4110ca7aecfc35db0c936dcd73da6b59ca1234
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-16 23:12:26 +0100

    modify

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,37 @@
 // routes/admin.js
 import express from "express";
 import User from "../models/User.js";
+import Admin from "../models/Admin.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { requireAdmin } from "../middleware/adminMiddleware.js";
 
+
 const router = express.Router();
+
+
+
+// Admin login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ email });
+    if (!admin) return res.status(401).json({ msg: "Admin not found" });
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) return res.status(401).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: admin._id, role: admin.role }, process.env.JWT_SECRET, { expiresIn: "1d" });
+
+    res.json({ token, role: admin.role });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+
+
 
 // PATCH /api/admin/update-user-stats/:id
 router.patch("/update-user-stats/:id", requireAdmin, async (req, res) => {
