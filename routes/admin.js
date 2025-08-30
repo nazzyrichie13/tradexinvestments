dd3bce0f155f3b79ae966fda34193de8d8a71ecf
nazzyrichie13 <youngnazzy13@gmail.com>
2025-08-30 12:37:41 +0100

    modify

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../models/User.js";
-import { requireAdmin } from "../middleware/auth.js";
+import { verifyToken } from "../routes/auth.js";
+ 
 
 const router = express.Router();
 
